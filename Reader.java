844750cd5a743bdd5acbd0ad270d2eefe17709e6
kalyanikattula <41290151+kalyanikattula@users.noreply.github.com>
2018-07-18 16:22:51 +0530

    Reader

---
+++ Reader.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class Reader
+{
+public static void main(String args[])throws Exception
+{
+BufferedReader br=new BufferedReader(new FileReader("kals.txt"));
+String a=br.readLine();
+Pattern p=Pattern.compile("[0-9]*[^a-zA-Z]");
+Matcher m=p.matcher(a);
+while(m.find())
+{
+System.out.println(m.group());
+}
+}
+}
