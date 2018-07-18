49b3da33cfb526aaa20365feefd374ce7cd86414
kalyanikattula <41290151+kalyanikattula@users.noreply.github.com>
2018-07-18 16:19:37 +0530

    Demo

---
+++ Demo.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+class Demo
+{
+public static void main(String args[])
+{
+ Pattern compile=Pattern.compile("[0-9]{2}[A-Z]{2}[0-9][A-Z][0-9]{4}");
+ Matcher matcher=compile.matcher("16EM1A0543");
+ while(matcher.find())
+ {
+  System.out.println(matcher.group());
+ }
+}
+} 
