8c17351ded22d01570607fa7a7fbcd14e5d13e34
kalyanikattula <41290151+kalyanikattula@users.noreply.github.com>
2018-07-18 16:13:27 +0530

    x pattern

---
+++ Star1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class Star1
+{
+public static void main(String args[])
+{
+int a[]={1,2,3,2,1};
+int l=a.length;
+for(int i=0;i<l;i++)
+{
+for(int j=0;j<l;j++)
+{
+if(j==i||j==(l-1-i))
+{
+System.out.print(a[j]);
+}
+else
+{
+System.out.print(" ");
+}
+}
+System.out.println(" ");
+}
+}
+}
+ 
